0618fc6bf2730e4d5c7d65cf61f588ae11609d32
SeanCarlos205 <16045@my.sanctamaria.school.nz>
2022-03-09 15:09:48 +1300

    Created Package
    Changed the file setup so that the program runs as a package

--- blog_app/main_app/routes.py
+++ blog_app/main_app/routes.py
@@ -1,37 +1,7 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '292ecfd3114ae58cffef6b2febc720e5'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')    
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.uctnow)
-    content = db.Column(db.Text, nullable = False)
-    usier_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}','{self.date_posted}')"
+from flask import render_template, url_for, flash, redirect
+from main_app import app
+from main_app.forms import RegistrationForm, LoginForm
+from main_app.models import User, Post
 
 
 posts = [
@@ -58,8 +28,6 @@ posts = [
     }
     
 ]
-
-app = Flask(__name__)
 
 #Route for home page
 @app.route("/")
@@ -92,6 +60,3 @@ def login():
         else:
             flash('Login failed. Please check your username and password', 'danger6')
     return render_template('login.html', title='Login', form='forms')
-
-if __name__ =='__main__':
-    app.run(debug=True)
